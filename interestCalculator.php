dc899f100025fddffba2773cb84fa7d5fc2fcee6
NatanRei <nreischmura@gmail.com>
2022-04-14 01:22:51 -0300

    verificações de padrão internacinal concluidas

--- interestCalculator.php
+++ interestCalculator.php
@@ -4,9 +4,18 @@ class Main
 {
     public function calc($value, $juros, $tempo){
 
-    if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$tempo)) {
+$test = explode(".",$value);
+if(strlen($test[1]) == 2){
+    echo "";
+}else{
     return false;
-}
+};
+
+
+    if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$tempo)) {
+        return false;
+};
+    
 
     $jurosForCalc = (int)$juros / 100;
     $soma = 1 + $jurosForCalc;
